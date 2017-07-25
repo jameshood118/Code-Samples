bb1b85751ffb21dc07c2d5ee7d36a7902184e140
James Hood <jameshood118@gmail.com>
2017-07-25 10:20:15 -0400

    Update ISSET.php

--- ISSET.php
+++ ISSET.php
@@ -1,5 +1,5 @@
-<?PHPif (isset($_POST['Edit'])) { 
-Code to be rendered only if it finds Edit in the POST Super global 
+<?PHP if (isset($_POST['Edit'])) { 
+//Code to be rendered only if it finds Edit in the POST Super global 
 }?> 
 
 <?PHP 
